f1d8f5d0082ccb160cbbd3d0abd1eddb9723c5b0
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-08-03 11:27:42 -0700

    cleanup run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 
 for i in {2501..5000}
 do
-   blenderproc run main-box-physics.py --output-dir output --id ${i}
-   # blenderproc extract hdf5 output/0.hdf5
-   # mv 0_colors.png ${i}_colors.png
-   # mv 0_depth.png ${i}_depth.png
-   # mv 0_instance_segmaps.png ${i}_instance_segmaps.png
+   blenderproc run main-box-physics.py --output-dir output --id ${i} --data-path /media/ws1/Data3/datasets/sps_synthetic
 done
